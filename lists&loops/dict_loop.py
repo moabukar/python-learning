bfd9a37d9ee2e6b4c27699d65db8238331fda9ee
mohamedA007 <mo-abukar10@hotmail.com>
2021-11-11 21:18:59 +0000

    .

---
+++ lists&loops/dict_loop.py
@@ -0,0 +1,7 @@
+# Looping over a dictionary
+
+student_grades = {"John": 6.5, "Will":4.5}
+
+for grades in student_grades.values():  # change grades.values to grades.keys for keys and same for the whole item (grade.items)
+        print(grades)
+
