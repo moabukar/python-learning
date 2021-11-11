8fe763c6fa44e4e82e9fc93317026ea91989a61b
mohamedA007 <mo-abukar10@hotmail.com>
2021-11-11 21:44:04 +0000

    using copy module

--- lists&loops/extras.py
+++ lists&loops/extras.py
@@ -1,5 +1,19 @@
+import copy
+
+
+# list and adding to list
+
 def eggs(somenumber):
     somenumber.append('Hello')
 
 spam = [1,2,3]
 eggs(spam)
+print(spam)
+
+# using copy (deepcopy) library to copy lists to new lists
+
+import copy
+
+cheese = copy.deepcopy(spam)
+eggs(cheese)
+print(cheese)
