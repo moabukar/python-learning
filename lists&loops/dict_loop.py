dd2fb250a65e65352723940606eeccc4b4350f6a
mohamedA007 <mo-abukar10@hotmail.com>
2021-11-16 20:29:43 +0000

    add: get keys of dictionaries & nested dictionary

--- lists&loops/dict_loop.py
+++ lists&loops/dict_loop.py
@@ -1,7 +1,31 @@
 # Looping over a dictionary
 
-student_grades = {"John": 6.5, "Will":4.5}
+# student_grades = {"John": 6.5, 
+#                   "Will":4.5,
+#                   "Josh":7.8}
 
-for grades in student_grades.values():  # change grades.values to grades.keys for keys and same for the whole item (grade.items)
-        print(grades)
+# for grades in student_grades.values():  # change grades.values to grades.keys for keys and same for the whole item (grade.items)
+#         print(grades)
 
+
+# json_loads = {'buildNumber': '1',
+#               'status':'completed',
+#               'result':'succeeded'
+# }
+
+# for json in json_loads.values():
+#         print(json) 
+
+## Get all keys of a nested dictionary
+
+def recursive_items(dictionary):
+    for key, value in dictionary.items():
+        if type(value) is dict:
+            yield from recursive_items(value)
+        else:
+            yield (key, value)
+
+a = {'a': {1: {1: 2, 3: 4}, 2: {5: 6}}}
+
+for key, value in recursive_items(a):
+    print(key, value)
